ffa96f34aa3b6b618418a5d03b6b4a6c95f06e43
wdl <weidongliang9@sjtu.edu.cn>
2024-02-11 11:25:01 +0800

    add detail
    Signed-off-by: wdl <weidongliang9@sjtu.edu.cn>

--- index.js
+++ index.js
@@ -205,25 +205,39 @@ app.post('/update-note', async (req, res) => {
 })
 
 app.post('/update-important-note', async (req, res) => {
-    // if (req.body.link !== null && req.body.isImportant === true){
+    if (req.body.link && req.body.isImportant === true){
 
-    //     const pageUrl = req.body.link;
-    //     const response = await fetch(pageUrl)
-    //     const data = await response.text()
-    //     const dom = new JSDOM(data)
-    //     const newDetail = dom.window.document.querySelector('.v_news_content').innerHTML
+        const pageUrl = req.body.link;
+        const response = await fetch(pageUrl)
+        const data = await response.text()
+        const dom = new JSDOM(data)
+        const newDetail = dom.window.document.querySelector('.v_news_content').textContent
 
-    //     Note.updateOne({_id : req.body._id},  { $set: { isImportant: req.body.isImportant , detail: newDetail} })
-    //     .then(() => res.redirect(web + "note"))
-    //     .catch(error => res.status(500).json({error}))
+        Note.updateOne({_id : req.body._id},  { $set: { isImportant: req.body.isImportant , detail: newDetail} })
+        .then(() => res.redirect(web + "note"))
+        .catch(error => res.status(500).json({error}))
 
-    // } else {
+    } else {
         Note.updateOne({_id : req.body._id},  { $set: { isImportant: req.body.isImportant } })
         .then(() => res.redirect(web + "note"))
         .catch(error => res.status(500).json({error}))
-    // }
+    }
     
 })
+
+// app.get('/api/detail', async (req, res) => {
+//     try {
+
+//         const pageUrl = "https://jwc.sjtu.edu.cn/info/1222/112981.htm";
+//         const response = await fetch(pageUrl)
+//         const data = await response.text()
+//         const dom = new JSDOM(data)
+//         const newDetail = dom.window.document.querySelector('.v_news_content').textContent
+//         res.json(newDetail);
+//     } catch (error) {
+//         res.status(500).json({error});
+//     }
+// });
 
 app.post('/delete-note', async (req, res) => {
     const id = req.body._id
