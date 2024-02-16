1badd5decb43e0c11b177473689cabfb10223872
wdl <weidongliang9@sjtu.edu.cn>
2024-02-16 15:17:59 +0800

    try to fix cors
    Signed-off-by: wdl <weidongliang9@sjtu.edu.cn>

--- mailer.js
+++ mailer.js
@@ -1,15 +1,15 @@
-// import nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer';
 
-// let nodeMail = nodemailer.createTransport({
-//     service: 'qq', 
-//     port: 465,
-//     secure: true,
-//     auth: {
-//         user: '2720609228@qq.com', 
-//         pass: 'itlthgqsxtgbddee' 
-//     }
-// });
+let nodeMail = nodemailer.createTransport({
+    service: 'qq', 
+    port: 465,
+    secure: true,
+    auth: {
+        user: '2720609228@qq.com', 
+        pass: 'itlthgqsxtgbddee' 
+    }
+});
 
-// // itlthgqsxtgbddee
+// itlthgqsxtgbddee
 
-// module.exports = {nodeMail};
+module.exports = nodeMail;
